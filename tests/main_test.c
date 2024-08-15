acfc139cb10d24a62ae8b595f4a0299cf7b46e86
Victor Chen <victor.chen@hoc-trade.com>
2024-08-15 20:26:47 +0800

    feature: successfully tokenize

--- tests/main_test.c
+++ tests/main_test.c
@@ -1,20 +1,27 @@
 // compile: gcc -o main_test main_test.c ../src/interpreter.c ../src/tokenizer.c ../src/parser.c
 // run it: ./main_test
 
+#include <stdio.h>
 #include "../src/tokenizer.h"
 #include "../src/parser.h"
 #include "../src/interpreter.h"
 
 void test_for_loop_interpreter() {
-    const char* input = "for (int i = 0; i < 10; i++) { print i; }";
-    Token* tokens = tokenize(input);
-    ASTNode* ast = parse(tokens);
+    const char* input = "for node in graph { doSomething(node); }";
+    char** tokens = tokenize(input);
 
-    interpret(ast);
+    int index = 0;
+    while (tokens[index] != NULL) {
+        printf("Token: %s\n", tokens[index]);
+        index++;
+    }
+    free_tokens(tokens);
+    // ASTNode* ast = parse(tokens);
 
-    // Clean up
-    free(tokens);
-    free(ast);
+    // interpret(ast); // engine
+
+    // free(tokens);
+    // free(ast);
 }
 
 int main() {
